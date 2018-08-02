9a2916a592f02d03d44e226a8de41aded81eae1f
Daniel Rourke <daniel.rourke@aussiebroadband.com.au>
2018-08-03 01:53:53 +1000

    prac 2.2

---
+++ prac2/prac2.2.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<iomanip>
+#include<ctime>
+
+
+using namespace std;
+
+int main() {
+	srand(time(0));
+	const int ROWS=3;
+	const int COLS=4;
+
+
+	int table[ROWS][COLS];
+	int sumOfZeros= 0, row , column;
+	
+
+	for (int i = 0; i < ROWS; i ++) {
+		for (int j = 0; j < COLS; j++)   
+		{
+			table[i][j] = rand()%3-1;
+			if (table[i][j] == 0)
+			{
+				sumOfZeros++;
+			}
+		}
+	}
+
+	for (int i = 0; i < ROWS; i ++)  {
+		for (int j = 0; j < COLS; j++)
+		{
+			cout << setw(3) << table[i][j];
+		}
+	
+		cout << endl;
+	 }
+
+	cout << "There are " << sumOfZeros << " is this table" << endl;
+	cout << "Please enter your Co-ords:" ;
+	cin >> row >> column;
+
+	for (int i = 0; i < ROWS; i ++)  {
+		for (int j = 0; j < COLS; j++)
+		{
+			if ( ( (j == column - 1 || j == column + 1 ) && (i == row) ) || ( (j == column) && (i == row - 1 || i == row + 1 ) ) )
+			{
+				cout << setw(3) << table[i][j];
+			}
+			else
+			{
+				cout << setw(3) << " ";
+			}
+		}
+	
+		cout << endl;
+	 }
+
+
+
+	return 0;
+}
