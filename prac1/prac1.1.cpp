79d96ad2634e2d3a0c1a3bf8d1fccd0150c671b8
Daniel Rourke <daniel.rourke@aussiebroadband.com.au>
2018-08-02 12:49:12 +1000

    1.1 complete

---
+++ prac1/prac1.1.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+# include <string>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    cout << "Please enter your name: " ;
+    cin >> name;
+    cout <<  "Greetings " << name << "!!!" << endl;
+
+    return 0;
+}
