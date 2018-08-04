aab287a643f2ee6df91980ddd03e74d187790511
Daniel Rourke <daniel.rourke@aussiebroadband.com.au>
2018-08-04 20:22:38 +1000

    prac 2.4 complete

--- prac2/prac2.4.cpp
+++ prac2/prac2.4.cpp
@@ -5,35 +5,42 @@
 using namespace std;
 
 
-int getSeatAllocation(char seats[][8])
+int getSeatAllocation(bool emptySeat[8][4])
 {
     bool allocated = false;
     int row = 0 , col = 0;
     while (!allocated)
     {
             cout << "Which seat would you reserve? :" << endl;
-            cin >> row, col;
-            if (seats[row][col] == '_')
+            cout <<  "Row  :   " ;
+            cin >> row;
+            cout << "Column:  " ;
+            cin >> col;
+            //if ( (row > 0) && (row < 9) && (col > 0) && (col > 5 ) &&  )
+            if ( row > 0 && row < 9 && col > 0 && col < 9)
             {
-                seats[row][col] = 'X';
-                allocated = true;
-                cout << "Seat " << row << " " << col << " has been reserved" << endl;
-             
+                if((emptySeat[row -1][col -1]))
+                {
+                    emptySeat[row - 1][col - 1] = false;
+                    allocated = true;
+                    cout << "Seat in Row :" << row << "  Column: " << col << " has been reserved" << endl;
+                }
+                else
+                {
+                    cout << "that seat is unaviable. Please select another" << endl;
+                    row = 0 , col = 0;
+                }
             }
             else
             {
-                 cout << "that seat is unaviable. Please select another" << endl;
-
+                cout << "Those seat numbers do not exist" << endl;
             }
-               
     }
 
     return 0;
 }
 
-bool vaildSelection(char)
-
-int displayAllocation(char seats[][8])
+void displayAllocation(bool emptySeat[8][4])
 {
     cout << "  1 2 3 4" << endl;
     for (int row = 0 ; row < 8; row++)
@@ -41,45 +48,46 @@ int displayAllocation(char seats[][8])
         cout << row + 1;
         for (int col = 0 ; col < 4 ; col++)
         {
-            cout << setw(2) << seats[row][col];
+            cout << setw(2);
+
+            if (emptySeat[row][col])
+                cout << "_";
+            else if (!(emptySeat[row][col]))
+                cout << "X";
         }
         cout << endl;
     }
-}
+    cout << endl;
 
-int intialiseSeats(char seats[][8])
+}
+void intitialiseArray(bool emptySeat[8][4])
 {
-    //char emptySeat = '_';
     for (int row = 0 ; row < 8; row++)
+    {
         for (int col = 0 ; col < 4 ; col++)
         {
-            seats[row][col] = '_';
-        }
-    return 0;
+            emptySeat[row][col] = true;
+        } 
+    }
 }
-
-
 int main()
 {
    
-    bool seats[4][8];
-    intialiseSeats(seats);
-    displayAllocation(seats);
+    bool emptySeat[8][4];
+  //  avilableSeats = 32
+  //  int passengers = 0;
+    intitialiseArray(emptySeat);
+    char ans  = 'y';
+
 
     for (int avilableSeats = 32; avilableSeats > 0; avilableSeats--)
     {
-        getSeatAllocation(seats);
-        displayAllocation(seats);
-
-
-        char ans  = 'y';
-        cout << avilableSeats << " seats remain do you wish to allocate another seat ? ( y / n) ";
+        displayAllocation(emptySeat);
+        getSeatAllocation(emptySeat);
+        cout <<" Empty Seats remain do you wish to allocate another seat ? ( y / n) ";
         cin >> ans;
-
         if (ans == 'n' || ans == 'N')
             break;
-           
-        
     }
     cout << "Allocation Complete" << endl;
     return 0;
